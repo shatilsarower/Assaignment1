090258b1cd73dcc4669a4fd862c3466d8502cc36
sms <shatild12@gmail.com>
2021-12-02 22:58:00 +0600

    Problem2

--- bin/assaignment1.dart
+++ bin/assaignment1.dart
@@ -1,4 +1,10 @@
-void main(List<String> arguments) {
-  print('Hello');
-  print('Shatil');
+import 'dart:io';
+void main() {
+int count=0;
+print("Enter the number\n");
+int a = int. parse(stdin. readLineSync()!);
+print("Enter the number\n");
+int b = int. parse(stdin. readLineSync()!);
+count = a+b;
+print("The sum of those number are \n $count",);
 }
