47af72bce2b415261948aeec61d3519848303275
sms <shatild12@gmail.com>
2021-12-02 23:46:40 +0600

    Problem4

--- bin/assaignment1.dart
+++ bin/assaignment1.dart
@@ -7,36 +7,45 @@ main() {
 // int b = int. parse(stdin. readLineSync()!);
 // count = a+b;
 // print("The sum of those number are \n $count",);
-
-
+print(product());
 }
 
-int mon(int a) {
-   a = 12;
-   return a;
-}
-String mon1(String a) {
-  a = '12';
-  return mon1(a);
-}
-String mon2(String a) {
-  a = 'Shatil';
-  return a;
-}
-double mon3(double a) {
-  a = 23.4;
-  return a;
-}
-List<String> mon4(String a){
-   List<String> a=['Shatil', 'Alauddin', 'Salma'] ;
-  return a;
-}
-List<int> mon5(int a){
-  int a=[1,2,3,4] as int;
-  return[a];
-}
+// int mon(int a) {
+//    a = 12;
+//    return a;
+// }
+// String mon1(String a) {
+//   a = '12';
+//   return mon1(a);
+// }
+// String mon2(String a) {
+//   a = 'Shatil';
+//   return a;
+// }
+// double mon3(double a) {
+//   a = 23.4;
+//   return a;
+// }
+// List<String> mon4(String a){
+//    List<String> a=['Shatil', 'Alauddin', 'Salma'] ;
+//   return a;
+// }
+// List<int> mon5(int a){
+//   int a=[1,2,3,4] as int;
+//   return[a];
+// }
+//
+// Map<String,String> mon6(String a){
+//   Map<String, String> a={'id':'110', 'name':'yourname'};
+//   return a;
+// } sf Problem p pefldshflkds
 
-Map<String,String> mon6(String a){
-  Map<String, String> a={'id':'110', 'name':'yourname'};
-  return a;
+product(){
+  int pro=1;
+  print("Enter the number\n");
+  int a = int. parse(stdin. readLineSync()!);
+  print("Enter the number\n");
+  int b = int. parse(stdin. readLineSync()!);
+  pro = a*b;
+  return pro;
 }
