be84779129468e33f7b5cd00334872b920f84a66
sms <shatild12@gmail.com>
2021-12-02 23:38:33 +0600

    Problem3

--- bin/assaignment1.dart
+++ bin/assaignment1.dart
@@ -1,10 +1,42 @@
 import 'dart:io';
-void main() {
-int count=0;
-print("Enter the number\n");
-int a = int. parse(stdin. readLineSync()!);
-print("Enter the number\n");
-int b = int. parse(stdin. readLineSync()!);
-count = a+b;
-print("The sum of those number are \n $count",);
+main() {
+// int count=0;
+// print("Enter the number\n");
+// int a = int. parse(stdin. readLineSync()!);
+// print("Enter the number\n");
+// int b = int. parse(stdin. readLineSync()!);
+// count = a+b;
+// print("The sum of those number are \n $count",);
+
+
+}
+
+int mon(int a) {
+   a = 12;
+   return a;
+}
+String mon1(String a) {
+  a = '12';
+  return mon1(a);
+}
+String mon2(String a) {
+  a = 'Shatil';
+  return a;
+}
+double mon3(double a) {
+  a = 23.4;
+  return a;
+}
+List<String> mon4(String a){
+   List<String> a=['Shatil', 'Alauddin', 'Salma'] ;
+  return a;
+}
+List<int> mon5(int a){
+  int a=[1,2,3,4] as int;
+  return[a];
+}
+
+Map<String,String> mon6(String a){
+  Map<String, String> a={'id':'110', 'name':'yourname'};
+  return a;
 }
